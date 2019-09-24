d890a8e6f7270c58dac67508e1d1fa939dbf86f1
ngjunjie01998 <55732112+ngjunjie01998@users.noreply.github.com>
2019-09-24 14:29:59 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+ 'TelegramProductionURL': 'https://thirdtpt.herokuapp.com/', // your heroku app do not forget trailing "/" !!
+ 'TelegramToken': '937633648:AAGMmfdJ1Uljp1kOin_-21_CwMhL93h-bVc', // Telegram token you got from @BotFather
+ 'googleSheetKey': '1BauxX4KDpqO7mEh0-JiBcCWTj9H4hljEiExN_Qwhs30', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
+ "confTimeZone": "Europe/Berlin" // time zone of the conference so the bot could output events for the current time in the form like "Europe/Berlin", see http://momentjs.com/timezone/
+}
