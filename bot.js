ebbd0a5ebfbce96dd11d29ef48cbf35e8f00fcce
JJ <ngjunjiecometj@gmail.com>
2019-09-29 20:14:49 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,15 +28,15 @@ bot.onText(/(.+)$/, function (msg, match) {
     // keywords are anything typed in
   var keywords = match[1];
   var request = require("request");
-      
-    // send request to retrieve the spreadsheet as the JSON 
+
+    // send request to retrieve the spreadsheet as the JSON
     request(url, function (error, response, body) {
         if (error || response.statusCode != 200) {
             console.log('Error: '+error); // Show the error
             console.log('Status code: ' + response.statusCode); // Show the error
             return;
         }
-        
+
         var parsed = JSON.parse(body);
         var list = NaN;
         if (!isNaN(keywords))   // isNaN returns false if the value is number
@@ -48,12 +48,12 @@ bot.onText(/(.+)$/, function (msg, match) {
                 list = NaN;
             }
         }
-        
+
         if (isNaN(list))
             list = -1;
-        
+
         var formattedAnswer = "";
-                
+
         var itemsFound = 0;
         // sending answers
         parsed.feed.entry.forEach(function(item){
@@ -66,39 +66,41 @@ bot.onText(/(.+)$/, function (msg, match) {
                 {
                     msge = NaN;
                 }
-                
+
                 if (
                     (!isNaN(msge) && msge == list) ||
                     (isNaN(msge) && itemTitle.toLowerCase().trim() == keywords.toLowerCase().trim())
                     )
                 {
                     // add the line break if not the first answer
-                    if (itemsFound==0) 
+                    if (itemsFound==0)
                         formattedAnswer += "";
-                    else 
+                    else
                         formattedAnswer += "\n\n";
-                        
+
                     itemsFound++;
                     formattedAnswer +=  item.content.$t; // add item content, '\u27a1' is the arrow emoji
                 }
-           
+
         });
-        
-        // if no items were found for the given time 
+
+        // if no items were found for the given time
         if (itemsFound == 0)
         {
             if (list<0 || list>24)
-                formattedAnswer = "";
-            else 
                 formattedAnswer = "Input /help for guidance";
-                
+            else
+                formattedAnswer = "Input /help for guidance";
+
         }
-    
+
         // send message telegram finally
+        var newanswer = formattedAnswer.replace("_cokwr: ","")
+:
         bot.sendMessage(msg.chat.id, formattedAnswer).then(function () {
             // reply sent!
         });
-    
+
     });
 
 });
