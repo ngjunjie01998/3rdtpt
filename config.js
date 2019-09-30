4c61a47af93e43bf211469a8d8d6a0a044312eea
JJ <ngjunjiecometj@gmail.com>
2019-10-01 07:55:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
  'TelegramProductionURL': 'https://thirdtpt.herokuapp.com/', // your heroku app do not forget trailing "/" !!
- 'TelegramToken': '937252584:AAGEDhnbquO7X8psXNf3oiePbDxGuPrw_IM', // Telegram token you got from @BotFather
+ 'TelegramToken': '931110491:AAHmAtqmL_Zt_VkJABnr7Ycyp3NqcyetLLo', // Telegram token you got from @BotFather
    'googleSheetKey': '1BauxX4KDpqO7mEh0-JiBcCWTj9H4hljEiExN_Qwhs30', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
  "confTimeZone": "Singapore" // time zone of the conference so the bot could output events for the current time in the form like "Europe/Berlin", see http://momentjs.com/timezone/
 }
